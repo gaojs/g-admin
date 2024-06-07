34b4960e9999e31d30fa726861de522f65132f2f
hill.gao <hill.gao@yuansuan.com>
2024-06-07 14:24:43 +0800

    前后端，测试通过！

--- web/src/view/person/person.vue
+++ web/src/view/person/person.vue
@@ -57,21 +57,21 @@
                 <el-tooltip
                   class="item"
                   effect="light"
-                  content="北京反转极光科技有限公司-技术部-前端事业群"
+                  content="浙江杭州"
                   placement="top"
                 >
                   <li class="info-list">
                     <el-icon>
                       <data-analysis />
                     </el-icon>
-                    北京反转极光科技有限公司-技术部-前端事业群
+                    浙江杭州
                   </li>
                 </el-tooltip>
                 <li class="info-list">
                   <el-icon>
                     <video-camera />
                   </el-icon>
-                  中国·北京市·朝阳区
+                  中国·杭州市
                 </li>
                 <el-tooltip
                   class="item"
